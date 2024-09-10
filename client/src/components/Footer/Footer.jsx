21c58f221c36f39a7dfb27af890028b6d6ac5dee
Kuldeep <kuldeepsinghtanwar2001@gmail.com>
2024-09-11 01:26:07 +0530

    footer height commit

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-black text-white py-6">
-      <div className="container mx-auto text-center">
+    <footer className="bg-black text-white py-6 h-32">
+      <div className="container mx-auto my-auto text-center">
         <p>
           Copyright &copy; {new Date().getFullYear()} Kuldeep | All Rights
           Reserved
