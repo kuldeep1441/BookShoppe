ea1c0a6942a5941a0c568c23d7bca07d05c94c96
Kuldeep <kuldeepsinghtanwar2001@gmail.com>
2024-09-11 01:32:18 +0530

    third commit

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-black text-white py-6 h-32 items-center justify-center">
+    <footer className="bg-black text-white py-6 items-center justify-center">
       <div className="container mx-auto my-auto text-center">
         <p>
           Copyright &copy; {new Date().getFullYear()} Kuldeep | All Rights
