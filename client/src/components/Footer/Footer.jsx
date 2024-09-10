388eca3c97bae9d56e8f41b63b6d0bb30fdb1d93
Kuldeep <kuldeepsinghtanwar2001@gmail.com>
2024-09-11 01:23:40 +0530

    footer2 commit

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -4,9 +4,10 @@ const Footer = () => {
   return (
     <footer className="bg-black text-white py-6">
       <div className="container mx-auto text-center">
-        <p className="text-lg font-semibold">BookShoope</p>
-        <p>Created by Kuldeep in 2024</p>
-        <p>&copy; {new Date().getFullYear()} All Rights Reserved</p>
+        <p>
+          Copyright &copy; {new Date().getFullYear()} Kuldeep | All Rights
+          Reserved
+        </p>
       </div>
     </footer>
   );
